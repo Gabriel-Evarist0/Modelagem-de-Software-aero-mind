422f7d384a137d6a0cd3b9873f4dd428e51b5e69
Gabriel-Evarist0 <machado.gabrielevaristo@gmail.com>
2022-09-14 14:33:23 -0300

    Commit 14/09/22 - 14:33

--- Files/aeromind.js
+++ Files/aeromind.js
@@ -1,9 +1,24 @@
-function verificaConta(){
-    var conta = "sim";
-    if (conta == "sim"){
+function verificaConta(conta){
+    if (conta == 'sim'){
         window.location.href = "/Files/login.html";
     }
     else{
         window.location.href = "https://youtube.com";
     }
+}
+function teste(){
+    var login = document.getElementById("input").value;
+    var senha = document.getElementById("inputSenha").value;
+    if (login == "Roberto" && senha == "123"){
+        document.getElementById("teste").innerHTML = "Roberto oi";
+    }else{
+        document.getElementById("teste").innerHTML = "Não é o Roberto";
+    }
+}
+
+function openNav(){
+    document.getElementById("menu").style.width = "250px"
+}
+function closeNav(){
+    document.getElementById("menu").style.width = "0px"
 }
