8d7d79abb1bca7e82a84db1860cf8e0fb4e0de4e
Gabriel-Evarist0 <machado.gabrielevaristo@gmail.com>
2022-09-14 11:37:55 -0300

    fodase

---
+++ Files/aeromind.js
@@ -0,0 +1,9 @@
+function verificaConta(){
+    var conta = "sim";
+    if (conta == "sim"){
+        window.location.href = "/Files/login.html";
+    }
+    else{
+        window.location.href = "https://youtube.com";
+    }
+}
